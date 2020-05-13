4003066b40eed9d10cd752a6f213ac93446b81c3
Nicola Hastings <nicolahastings@gmail.com>
2020-05-13 15:12:02 -0400

    NH | adding background image JS, JS for FOUC, search bar JS, slick init and smooth scroll JS

---
+++ fouc.js
@@ -0,0 +1,1 @@
+$("body").css("visibility","hidden"),$(document).ready(function(){$("body").css("visibility","visible").hide().fadeIn("slow")});
---
+++ smooth-scroll.js
@@ -0,0 +1,1 @@
+function topOfPage(){$("body,html").animate({scrollTop:0},500)}function scrollToLocation(o){var t=$(o).offset().top;$("body,html").animate({scrollTop:t},500)}
